4f47e1e308d684e4d5197ae57d7e247fdb32487d
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-09-18 10:37:55 +0700

    Add files via upload

---
+++ tugas1.js
@@ -0,0 +1,28 @@
+// var nmdepan = "hasan";
+// var nmbelakang = "habsyi";
+// var umur = 17
+
+// console.log ("nama depan :",nmdepan, 
+// 			"nama belakang :",nmbelakang, 
+// 			"usia :", umur)
+
+//==========================================
+
+// const nmdepan = "hasan";
+// const nmbelakang = "habsyi";
+// const umur = 17
+
+// console.log ("nama depan :",nmdepan, 
+// 			"nama belakang :",nmbelakang, 
+// 			"usia :", umur)
+
+
+//==========================================
+
+var nmdepan = "hasan";
+let nmbelakang = "habsyi";
+const umur = 17
+
+console.log ("nama depan :",nmdepan, 
+			"nama belakang :",nmbelakang, 
+			"usia :", umur)
